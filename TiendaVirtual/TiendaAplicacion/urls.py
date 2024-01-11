4af4c152e1e1ebd600527806229bc5be3ab15002
Xevhra <garciarodriguezalvaro12@gmail.com>
2024-01-11 22:56:44 +0100

    CRUD (Base añadida, terminar templates)

--- TiendaVirtual/TiendaAplicacion/urls.py
+++ TiendaVirtual/TiendaAplicacion/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import *
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("admin/listado", ListaProductos.as_view(), name="listadoProductos"),
+    path("admin/libro_detalle/<int:pk>/", DetalleProducto.as_view(), name="detalleProducto"),
+    path("admin/libro_detalle/editar/<int:pk>", UpdateProducto.as_view(), name="actualizarProductos"),
+    path("admin/listado/crear_libro", CreateBookView.as_view(), name="crearProductos"),
+    path("admin/listado/<pk>/borrar", ProductoDeleteView.as_view(), name="borrarProductos"),
 ]
--- TiendaVirtual/TiendaAplicacion/views.py
+++ TiendaVirtual/TiendaAplicacion/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
+from .models import *
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world.")
+class ListaProductos (ListView):
+    model = Producto
+    template_name = 'admin/listado.html'
+    
+class DetalleProducto(DetailView):
+    model = Producto
+    template_name = 'admin/libro_detalle.html'    
+    context_object_name = 'producto'
+
+class UpdateProducto(UpdateView):
+    model = Producto
+    template_name = 'admin/editar_producto.html'
+    
+class CreateBookView(CreateView):
+    model = Producto
+    template = 'admin/Create_Producto.html'
+    fields = ['nombre', 'modelo', 'unidades', 'precio', 'vip']
+    
+class ProductoDeleteView(DeleteView):
+    model = Producto
+    success_url = reverse_lazy('listadoProductos')
+    template_name = "admin/confirmacionborrado.html"
+            
