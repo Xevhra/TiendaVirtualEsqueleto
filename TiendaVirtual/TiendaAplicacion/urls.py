894dd1ba94fc00b9c35821be0e7ac089cddcca2f
Xevhra <garciarodriguezalvaro12@gmail.com>
2024-01-12 10:48:00 +0100

    commit inicial prueba

--- TiendaVirtual/TiendaAplicacion/urls.py
+++ TiendaVirtual/TiendaAplicacion/urls.py
@@ -4,10 +4,9 @@ from .views import *
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("admin/listado", ListaProductos.as_view(), name="listadoProductos"),
     path("admin/libro_detalle/<int:pk>/", DetalleProducto.as_view(), name="detalleProducto"),
-    path("admin/libro_detalle/editar/<int:pk>", UpdateProducto.as_view(), name="actualizarProductos"),
-    path("admin/listado/crear_libro", CreateBookView.as_view(), name="crearProductos"),
-    path("admin/listado/<pk>/borrar", ProductoDeleteView.as_view(), name="borrarProductos"),
+    path("admin/edicion/<int:pk>", UpdateProducto.as_view(), name="actualizarProductos"),
+    path("admin/listado/nuevo", CreateBookView.as_view(), name="crearProductos"),
+    path("admin/listado/eliminar/<int:pk>", ProductoDeleteView.as_view(), name="borrarProductos"),
 ]
